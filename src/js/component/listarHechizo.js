79820fed8a7c74a5f33537015f7e6c5d7a1d6bfd
Pedro López Murúa <pedro.lopez@prologic.cl>
2023-04-01 22:14:12 +0000

    card hechizos

--- src/js/component/listarHechizo.js
+++ src/js/component/listarHechizo.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "../../styles/styles.css";
 
 const hechizos = [
   {
     nombre: "Expecto Patronum",
     descripcion: "Hechizo utilizado para repeler a los dementores.",
+    nivel: "Intermedio",
+  },
+  {
+    nombre: "Aberto",
+    descripcion: "Opens locked doors",
+    nivel: "Intermedio",
+  },
+  {
+    nombre: "Accio",
+    descripcion: "Summons objects",
     nivel: "Intermedio",
   },
   {
@@ -15,14 +26,27 @@ const hechizos = [
 
 const ListarHechizo = () => {
   return (
-    <div>
-      {hechizos.map((hechizo) => (
-        <div key={hechizo.nombre}>
-          <h2>{hechizo.nombre}</h2>
-          <p>{hechizo.descripcion}</p>
-          <p>Nivel: {hechizo.nivel}</p>
-        </div>
-      ))}
+    <div className="container">
+      <div className="row justify-content-center">
+        {hechizos.map((hechizo) => (
+          <div className="col-md-4 mb-4" key={hechizo.nombre}>
+            <div className="card h-100">
+              <div className="card-header">
+                <h2>{hechizo.nombre}</h2>
+              </div>
+              <div className="card-body">
+                <p className="card-text">Descripción: {hechizo.descripcion}</p>
+                <p className="card-text">Nivel: {hechizo.nivel}</p>
+              </div>
+              <div className="card-footer">
+                <a href="/" type="button" className="btn btn-outline-secondary btn-lg">
+                  Volver
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
